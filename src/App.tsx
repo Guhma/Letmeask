d2f5db59983810de491e79496eeaaedcc51f169e
Gustavo Almeida <gustavoalmeida123490@gmail.com>
2021-11-14 18:09:54 -0300

    Integração com o Firebase e Autenticação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { Button } from "./components/Button";
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/Home";
 
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+  
   return (
-    <Home />
+    <BrowserRouter>
+    <AuthContextProvider>
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/rooms/new" component={NewRoom} />
+    </AuthContextProvider>
+</BrowserRouter>
+   
   );
 }
 
